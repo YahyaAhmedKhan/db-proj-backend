75962fff96fd4c9f14ec704fab330a5142894e3d
YahyaAhmedKhan <yahyaahmed.52@gmail.com>
2023-12-09 16:02:46 +0500

    added commit to transaction for booking

--- routes/booking.js
+++ routes/booking.js
@@ -3,17 +3,6 @@ const db = require('../db') // Adjust the path as necessary
 
 const router = express()
 
-// router.post('/addFlightbooking', async (req, res) => {
-//   try {
-//     const { accountId, flightRecordId } = req.body
-//     const insertedFlightBooking = await insertFlightBooking(accountId, flightRecordId)
-//     res.status(200).json(insertedFlightBooking) // Send the inserted row as JSON response
-//   } catch (error) {
-//     console.error('Error adding flight booking:', error)
-//     res.sendStatus(500)
-//   }
-// })
-
 router.post('/makeBooking', async (req, res) => {
   try {
     const { accountId, flightRecordId, passengerDetails } = req.body
@@ -21,22 +10,34 @@ router.post('/makeBooking', async (req, res) => {
     res.status(200).json(flightResults) // Send the inserted row as JSON response
   } catch (error) {
     console.error('Error adding flight booking:', error)
-    res.sendStatus(500)
+    res.status(500).json({ error: error.message })
   }
 }
 )
-
 async function makeBooking (accountId, flightRecordId, passengerDetails) {
-  const flightBooking = await insertFlightBooking(accountId, flightRecordId)
-  const flightBookingId = flightBooking.flight_booking_id
+  try {
+    // Start a transaction
+    await db.query('BEGIN')
 
-  const seatBookings = []
-  for (let i = 0; i < passengerDetails.length; i++) {
-    const seatBooking = await insertSeatBooking(passengerDetails[i], flightBookingId, i + 1)
-    seatBookings.push(seatBooking)
+    const flightBooking = await insertFlightBooking(accountId, flightRecordId)
+    const flightBookingId = flightBooking.flight_booking_id
+
+    const seatBookings = []
+    for (let i = 0; i < passengerDetails.length; i++) {
+      const seatBooking = await insertSeatBooking(passengerDetails[i], flightBookingId, i + 1)
+      seatBookings.push(seatBooking)
+    }
+
+    // Commit the transaction
+    await db.query('COMMIT')
+
+    return [flightBooking, seatBookings]
+  } catch (error) {
+    // Rollback the transaction in case of an error
+    await db.query('ROLLBACK')
+    console.error('Transaction failed:', error)
+    throw error
   }
-
-  return [flightBooking, seatBookings]
 }
 
 async function insertFlightBooking (accountId, flightRecordId) {
@@ -45,7 +46,6 @@ async function insertFlightBooking (accountId, flightRecordId) {
       'INSERT INTO flight_bookings (total_seats, total_price, account_id, flight_record_id) VALUES ($1, $2, $3, $4) RETURNING *',
       [0, 0, accountId, flightRecordId]
     )
-    // flightBooking.rows[0] contains the newly inserted row
     const insertedFlightBooking = flightBooking.rows[0]
 
     console.log('Inserted flight booking:', insertedFlightBooking)
@@ -55,17 +55,6 @@ async function insertFlightBooking (accountId, flightRecordId) {
     throw error
   }
 }
-
-// router.post('/addSeatbooking', async (req, res) => {
-//   try {
-//     const { seatDeatils, flightBookingId, seatBookingNumber } = req.body
-//     const insertedSeatBooking = await insertSeatBooking(seatDeatils, flightBookingId, seatBookingNumber)
-//     res.status(200).json(insertedSeatBooking) // Send the inserted row as JSON response
-//   } catch (error) {
-//     console.error('Error adding seat booking:', error)
-//     res.sendStatus(500)
-//   }
-// })
 
 async function insertSeatBooking (seatDeatils, flightBookingId, seatBookingNumber) {
   const seatNumber = generateRandomSeatNumber()
